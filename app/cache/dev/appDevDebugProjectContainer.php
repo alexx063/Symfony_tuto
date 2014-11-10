9a06a60623c7cc59773bd824d0fcbfb63134abb6
alex063 <alexxmaries@gmail.com>
2014-11-10 11:35:23 +0100

    [testCommit]

--- app/cache/dev/appDevDebugProjectContainer.php
+++ app/cache/dev/appDevDebugProjectContainer.php
@@ -4023,8 +4023,8 @@ class appDevDebugProjectContainer extends Container
             ),
             'assetic.java.bin' => '/usr/bin/java',
             'assetic.node.bin' => '/usr/bin/node',
-            'assetic.ruby.bin' => 'C:\\RailsInstaller\\Ruby1.9.3\\bin\\ruby.EXE',
-            'assetic.sass.bin' => 'C:\\RailsInstaller\\Ruby1.9.3\\bin\\sass.BAT',
+            'assetic.ruby.bin' => '/usr/bin/ruby',
+            'assetic.sass.bin' => '/usr/bin/sass',
             'assetic.filter.cssrewrite.class' => 'Assetic\\Filter\\CssRewriteFilter',
             'assetic.twig_extension.functions' => array(
 
